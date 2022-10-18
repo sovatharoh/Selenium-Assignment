df958ef77de1485f669c82d72ae5c96546bb58d6
sovatharo-bt <SovatharoHuy32@gmail.com>
2022-10-18 18:43:08 +1100

    working test unique events near me

--- src/test/java/au/edu/rmit/ct/TryBookingTest_S3783867.java
+++ src/test/java/au/edu/rmit/ct/TryBookingTest_S3783867.java
@@ -8,17 +8,51 @@
 package au.edu.rmit.ct;
 
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 
 // Update this class name by replacing S3214321 with your student ID
 class TryBookingTest_S3783867 {
     WebDriver myDriver;
 
+    @Test
+    @Order(1)
+    @DisplayName("Try Booking check 12 or more unique Events near me")
+    void checkEventsNearMe() {
+        String url = "https://www.trybooking.com/book/search";
+        myDriver.get(url);
 
+
+        WebElement nearbyEventsContainer = myDriver.findElement(By.xpath("//*[@id=\"nearby-events-container\"]"));
+        List<WebElement> nearbyEvents = nearbyEventsContainer.findElements(By.xpath("./child::*"));
+
+        //Add to hashset, that stores unique values which means there can not be any duplicate events.
+        HashSet nearbyEventIds = new HashSet();
+        for ( WebElement i : nearbyEvents) {
+            nearbyEventIds.add(i.getAttribute("id"));
+        }
+        assertTrue(nearbyEventIds.size() >= 12);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Try Booking an event")
+    void checkCreateBooking() {
+        String url = "https://www.trybooking.com/CDKGX";
+        myDriver.get(url);
+        List<WebElement> lweA = myDriver.findElements(By.tagName("button"));
+        System.out.println(lweA);
+
+        fail("TODO");
+    }
     @Test
     // @Disabled
     @Order(0)
