78e3c965177195d4750d089ddccff22b6db9d96b
sovatharo-bt <SovatharoHuy32@gmail.com>
2022-10-19 17:07:07 +1100

    final commit, completed all tasks in rubric

--- src/test/java/au/edu/rmit/ct/JPetstoreTest_S3783867.java
+++ src/test/java/au/edu/rmit/ct/JPetstoreTest_S3783867.java
@@ -1,7 +1,7 @@
 /**
  *
- * Name: John Smith (( Update with your name here ))
- * Student ID: s45045012  (( Update with your ID))
+ * Name: Sovatharo Huy
+ * Student ID: s3783867  (( Update with your ID))
  *
  * [OPTIONAL: add any notes or comments here about the code]
  */
@@ -12,7 +12,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -21,58 +20,11 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 
 // Update this class name by replacing S3214321 with your student ID
-class JPetstoreTest_S3783867 {
+class zJPetstoreTest_S3783867 {
     WebDriver myDriver;
 
     @Test
-    @Order(2)
-    @DisplayName("Check Price for Adult Male Chihuahua at product page")
-    void checkChihuahua1() {
-
-        String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-26";
-        myDriver.get(chihuahuaURL);
-
-        // WebElement class represents an html tag such as <p> , <a> , <img> <td> for tables etc. even forms
-        // ... and this class has methods for you to check content and attributes
-        // see the getText() method below to read the text between the opening/closing tags.
-        // see the getAttribute("href") method which is used by toString() to read the url
-
-        List<WebElement> lweA = myDriver.findElements(By.tagName("a"));
-        System.out.println("Printing text from <a> elements:");
-        for (WebElement wea : lweA){
-            System.out.print(wea.getText() + ",");
-        }
-        System.out.println("Printing href attribute values from <a> elements:");
-        for (WebElement wea : lweA){
-            System.out.println(wea.getAttribute("href") + ",");
-        }
-
-        // for this web page (very 1990s) a lot of content are in tables so <td> is a standard cell in the table
-
-        List<WebElement> lweTD = myDriver.findElements(By.tagName("td"));
-        System.out.println("Printing text from <td> elements:");
-        for (WebElement wea : lweTD){ // each of this are td WebElements
-            System.out.print(wea.getText() + ", ");
-        }
-
-        // Thread.sleep() is not normally encouraged, but is a quick way to pause browser
-        // When you are processing a webpage with Selenium, if the webpage doesn't load fast enough
-        // an exception can be thrown unless you put in some wait time.
-        // There are more official ways to handle wait time - we will look at that next week.
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        // Write the tests for these below using JUnit assertEquals and the findElements method
-        fail("Task 11.1 : Check that the price is $125.50");
-        fail("Task 11.2 : Check that the product name is correct (Adult Male Chihuahua) for this product page");
-        fail("Task 11.3 : Check that the adult male chihuahua is in stock. ( > 0 ) - or perhaps this should be your first check before 11.1!?");
-
-    }
-
-    @Test
-    @Order(3)
+    @Order(1)
     @DisplayName("Check the pet name, price and check if there is stock for one pet of your choice.")
     void checkFemaleGoldFish() throws Exception {
         String femaleGoldFishURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-21";
@@ -106,44 +58,9 @@ class JPetstoreTest_S3783867 {
          * You will be asked to submit this for your Assignment 3 .
          */
     }
-    @Test
-    @Order(4)
-    @DisplayName("More examples with Xpath")
-    void checkChihuahua2(){
-
-        String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-26";
-        myDriver.get(chihuahuaURL);
-
-        /**
-         * If you have time you can look further with this below.
-         * Xpath has powerful functionality to find elements and values
-         * Xpath is a bit like regular expressions for html elements (DOM tree)
-         */
-
-        WebElement we; // to store the result of our search. Of course you can also use a List of WebElements if you expect a return of many results
-
-        // In this example we are searching only for the first td element, which has exact text value - '$125.50'
-        we = myDriver.findElement(By.xpath("//td[text()='$125.50']"));
-
-        // In this example we are searching only for any element, which contains a particular string value
-        // (a partial match, like a substring match)
-        // Here we are using * to check all elements for their enclosed value
-        // <p>Like the value here</p>
-        we = myDriver.findElement(By.xpath("//*[contains(text(),'$125')]"));
-
-        System.out.println("we.toString(): " + we.toString()); // see what it looks like toString()
-        System.out.println("we.getText(): " + we.getText()); // see what the text is
-
-        /**
-         * we.toString(): [[FirefoxDriver: firefox on WINDOWS (955939a2-3c3d-4cf3-b531-05dc9df88c99)] -> xpath: //*[contains(text(),'$125')]]
-         * we.getText(): $125.50
-         *
-         */
-
-    }
 
     @Test
-    @Order(5)
+    @Order(2)
     @DisplayName("Start a menagerie! Select a specific fish, specific cat, and a third pet (they will have a unique item ID)." +
             " Add 3 multiples of the fish, 2 multiples of the cat, and one of the third pet to the cart. Check the subtotal matches the expected price")
     void startAMenagerie() throws Exception {
--- src/test/java/au/edu/rmit/ct/TryBookingTest_S3783867.java
+++ src/test/java/au/edu/rmit/ct/TryBookingTest_S3783867.java
@@ -1,9 +1,12 @@
 /**
  *
- * Name: John Smith (( Update with your name here ))
- * Student ID: s45045012  (( Update with your ID))
+ * Name: Sovatharo Huy
+ * Student ID: s3783867
  *
- * [OPTIONAL: add any notes or comments here about the code]
+ * Could not select input for the <select></select> tags directly because the display was set to none. Instead, simulated user clicks on
+ * value. Could have simulated scroll down to select the wanted, but chose to select the option that was immediately seen.
+ * Had to add sleep thread between each select click, implicit wait was unreliable.
+ * The longer implicit waits, was to wait for the page to load which often took a long time.
  */
 package au.edu.rmit.ct;
 
@@ -11,6 +14,7 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.HashSet;
 import java.util.List;
@@ -39,6 +43,8 @@ class TryBookingTest_S3783867 {
         for ( WebElement i : nearbyEvents) {
             nearbyEventIds.add(i.getAttribute("id"));
         }
+
+        // if size >= then there are more than 11 unique event ids present
         assertTrue(nearbyEventIds.size() >= 12);
     }
 
@@ -48,10 +54,104 @@ class TryBookingTest_S3783867 {
     void checkCreateBooking() {
         String url = "https://www.trybooking.com/CDKGX";
         myDriver.get(url);
-        List<WebElement> lweA = myDriver.findElements(By.tagName("button"));
-        System.out.println(lweA);
+        myDriver.findElement(By.xpath("//*[@id=\"sticky-bottom-btn\"]/div/button")).click();
+        myDriver.findElement(By.xpath("//*[@id=\"event-app\"]/div[2]/div/div/div[2]/div[1]/div/div[5]/a")).click();
+        myDriver.findElement(By.xpath("//*[@id=\"ticket-rows-container\"]/div/div[1]/div[4]/div/button[2]")).click();
+        myDriver.findElement(By.xpath("//*[@id=\"tickets-next-button\"]")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        fail("TODO");
+        //Fill in student number input
+        myDriver.findElement(By.xpath("//*[@id=\"bookingDataForm\"]/div/div/div[1]/div[2]/input")).clear();
+        myDriver.findElement(By.xpath("//*[@id=\"bookingDataForm\"]/div/div/div[1]/div[2]/input")).sendKeys("s3783867");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //day select
+        myDriver.findElement(By.xpath("//*[@id=\"bookingDataForm\"]/div/div/div[2]/div[2]/div[1]/div[1]/div[1]/span[1]")).click();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        myDriver.findElement(By.xpath("//*[@id=\"bookingDataForm\"]/div/div/div[2]/div[2]/div[1]/div[1]/div[2]/div[2]/div[5]")).click();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //month select
+        myDriver.findElement(By.xpath("//*[@id=\"bookingDataForm\"]/div/div/div[2]/div[2]/div[1]/div[2]/div[1]/span[1]")).click();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        myDriver.findElement(By.xpath("//*[@id=\"bookingDataForm\"]/div/div/div[2]/div[2]/div[1]/div[2]/div[2]/div[2]/div[3]")).click();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //year select
+        myDriver.findElement(By.xpath("//*[@id=\"bookingDataForm\"]/div/div/div[2]/div[2]/div[1]/div[3]/div[1]/span[1]")).click();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        myDriver.findElement(By.xpath("//*[@id=\"bookingDataForm\"]/div/div/div[2]/div[2]/div[1]/div[3]/div[2]/div[2]/div[7]")).click();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //last semester select
+        myDriver.findElement(By.xpath("//*[@id=\"bookingDataForm\"]/div/div/div[3]/div[2]/div[1]/div[1]/span[1]")).click();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        myDriver.findElement(By.xpath("//*[@id=\"bookingDataForm\"]/div/div/div[3]/div[2]/div[1]/div[2]/div[2]/div[4]")).click();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        myDriver.findElement(By.xpath("//*[@id=\"bookingDataForm\"]/div/div/div[3]/div[2]/div[1]/div[1]/span[1]")).click();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        myDriver.findElement(By.xpath("//*[@id=\"data-entry-next-button\"]")).click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        myDriver.findElement(By.id("txtFirstName")).clear();
+        myDriver.findElement(By.id("txtFirstName")).sendKeys("Sovatharo");
+        myDriver.findElement(By.id("txtLastName")).clear();
+        myDriver.findElement(By.id("txtLastName")).sendKeys("Huy");
+        myDriver.findElement(By.id("txtEmailAddress")).clear();
+        myDriver.findElement(By.id("txtEmailAddress")).sendKeys("s3783867@student.rmit.edu.au");
+        myDriver.findElement(By.id("txtConfirmEmailAddress")).clear();
+        myDriver.findElement(By.id("txtConfirmEmailAddress")).sendKeys("s3783867@student.rmit.edu.au");
+        myDriver.findElement(By.xpath("//*[@id=\"btn-purchase-lg\"]")).click();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertEquals("Transaction successful", myDriver.findElement(By.xpath("/html/body/div[1]/div/main/div/div[1]/div[1]/h1/span")).getText());
+        assertEquals("s3783867@student.rmit.edu.au", myDriver.findElement(By.xpath("/html/body/div[1]/div/main/div/div[1]/div[2]/div/div[1]/div[2]/div[3]/span")).getText());
     }
     @Test
     // @Disabled
@@ -62,6 +162,7 @@ class TryBookingTest_S3783867 {
         String url = "https://www.trybooking.com";
         myDriver.get(url);
         assertEquals("Event Ticketing & Booking Platform | TryBooking Australia", myDriver.getTitle());
+
     }
 
     @BeforeEach
